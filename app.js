ca89a7a7d1086a6b4444e65dc38d374a87a85ea8
coolourcoin <yrsbrz2qbj@privaterelay.appleid.com>
2025-11-13 15:19:52 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// future small helpers can live here (active link, scroll, etc.)
-(function(){
-  const links = document.querySelectorAll('.cc-menu a');
-  links.forEach(a=>{
-    if (a.getAttribute('href') && location.pathname.endsWith(a.getAttribute('href'))) {
-      a.classList.add('is-active');
-    }
-  });
-})();
